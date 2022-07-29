a9daa7576b1a4fc5f8a95f6ada20d924c09d2898
Mirella Arguelles <marguelles@up.edu.ph>
2022-07-29 20:44:45 +0800

    feat: quality control using fastqc

---
+++ 02-quality_control/fastqc.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+
+################################
+# Quality control using FastQC #
+################################
+
+
+ARGUMENTS=2
+FORCE_EXIT=65
+EXT=".fastq"
+R1_ID="_1"
+R2_ID="_2"
+
+if [ $# -ne "$ARGUMENTS" ]
+then
+  echo "Looking for one argument: -r/--rloc"
+  exit $FORCE_EXIT
+fi
+
+# checking -r/--reads
+# Optional parameters
+#       1. -e/--ext: for changing extension
+#       2. --r1: for changing identifier for read 1
+#       3. --r2: for changing identifier for read 2
+while [ "$1" != "" ]; do
+  case $1 in
+    -r | --rloc )
+      shift
+      READS_LOCATION="$1"
+      ;;
+    -e | --ext )
+      shift
+      EXT="$1"
+      ;;
+    --r1 )
+      shift
+      R1_ID="$1"
+      ;;
+    --r2 )
+      shift
+      R2_ID="$1"
+      ;;
+    * )
+      echo "Unknown argument: $1"
+      exit 1
+  esac
+  shift
+done
+
+mkdir -p fastqc_outputs
+
+for r in $(ls $READS_LOCATION*$R1_ID$EXT)
+do
+    # echo $r
+    FILE_W_EXT=${r#$READS_LOCATION/}
+    FILENAME=${FILE_W_EXT%$R1_ID$EXT}
+    # echo $FILE_W_EXT
+    # echo $FILENAME
+    fastqc $FILENAME*$EXT -o fastqc_outputs 
+done
+
+if [ $(ls -A ./fastqc_outputs) ]
+then
+    for out in fastqc_outputs/*.zip 
+    do
+        unzip $filename -d fastqc_outputs/ # unzip and direct the output to the fastqc directory
+    done
+fi
